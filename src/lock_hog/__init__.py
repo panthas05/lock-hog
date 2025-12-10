8a683a99b87058ad2979618da222d7e12d0958b6
panthas <>
2025-12-10 06:39:08 +0000

    Initial commit - minimum build of the package
    Supports hogging locks from threads only.

---
+++ src/lock_hog/__init__.py
@@ -0,0 +1,7 @@
+from .hog_lock import hog_lock
+from .lock_hogger import LockHogger
+
+__all__ = [
+    "hog_lock",
+    "LockHogger",
+]
---
+++ tests/test_hog_lock.py
@@ -0,0 +1,95 @@
+import threading
+import time
+import unittest
+
+import lock_hog
+from lock_hog.hog_lock import HoggerStillAlive
+
+
+class CouldNotAcquireLock(Exception):
+    pass
+
+
+class ThreadLockHogger(lock_hog.LockHogger):
+    def __init__(
+        self,
+        *,
+        lock: threading.Lock,
+    ) -> None:
+        self.lock = lock
+
+    def acquire_lock(self) -> None:
+        acquired_lock = self.lock.acquire(blocking=False)
+        if not acquired_lock:
+            raise CouldNotAcquireLock
+
+    def release_lock(self) -> None:
+        self.lock.release()
+
+
+class SlowReleaseThreadLockHogger(ThreadLockHogger):
+    """
+    The same as ThreadLockHogger, but waits for a second before releaseing the lock. We
+    do this only so that we can test the timeout handling.
+    """
+
+    def release_lock(self) -> None:
+        time.sleep(1.0)
+        return super().release_lock()
+
+
+class TestHogLock(unittest.TestCase):
+    def test_acquires_lock_on_entry_and_releases_lock_on_exit(self) -> None:
+        lock = threading.Lock()
+
+        lock_hogger = ThreadLockHogger(lock=lock)
+
+        # safety check
+        self.assertFalse(
+            lock.locked(),
+            msg="Lock acquired before hog_lock context block was entered.",
+        )
+
+        with lock_hog.hog_lock(lock_hogger=lock_hogger):
+            self.assertTrue(
+                lock.locked(),
+                msg="Lock not acquired when within hog_lock context block.",
+            )
+
+        self.assertFalse(
+            lock.locked(),
+            msg="Lock not released after exiting hog_lock context block.",
+        )
+
+    def test_raises_lock_hogging_thread_is_still_alive_after_timeout(self) -> None:
+        slow_release_lock_hogger = SlowReleaseThreadLockHogger(lock=threading.Lock())
+
+        passed_timeout = 0.01
+
+        with self.assertRaisesRegex(
+            HoggerStillAlive,
+            (
+                "The thread that hogged the lock was still alive after "
+                f"{passed_timeout} seconds."
+            ),
+        ):
+            with lock_hog.hog_lock(
+                lock_hogger=slow_release_lock_hogger,
+                timeout=passed_timeout,
+            ):
+                pass
+
+    def test_hogger_still_alive_exception_message_with_one_second_timeout(self) -> None:
+        slow_release_lock_hogger = SlowReleaseThreadLockHogger(lock=threading.Lock())
+
+        passed_timeout = 1.0
+
+        with self.assertRaisesRegex(
+            HoggerStillAlive,
+            "The thread that hogged the lock was still alive after 1 second.",
+        ):
+            with lock_hog.hog_lock(
+                lock_hogger=slow_release_lock_hogger,
+                timeout=passed_timeout,
+            ):
+                pass
